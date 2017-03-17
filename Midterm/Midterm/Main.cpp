12eb8927f80db78e8c9676237b4a6efa40e18623
Game110AM <Game110AM@605-110-STU19>
2017-03-17 09:55:24 -0600

    commit

--- Midterm/Midterm/Main.cpp
+++ Midterm/Midterm/Main.cpp
@@ -46,20 +46,20 @@ int main() {
 		case 3:
 			cout << "you're in room 3! you can go east or west!";
 			getline(cin, input);
-			if (input == 'e')
+			if (input.compare("east") == 0)
 				room = 4;
-			if (input == 'w')
+			if (input.compare("west") == 0)
 				room = 2;
 			break;
 
 		case 4:
 			cout << "you're in room 4, where you found a sword! you can go west or south!";
 			getline(cin, input);
-			if (input == 'w')
+			if (input.compare ("west") == 0)
 				room = 3;
-			if (input == 's')
+			if (input.compare("south") == 0)
 				room = 6;
-			if (input == 'n')
+			if (input.compare("north") == 0)
 				room = 5;
 
 			for (int i = 0; i < 5; i++)
@@ -74,55 +74,55 @@ int main() {
 				
 			cout << "you can go south!" << endl;
 			getline(cin, input);
-			if (input == 's')
+			if (input.compare("south") == 0)
 				room = 6;
 
-			cout <<
+			cout << "no" << endl;
 
 			break;
 
 		case 6:
 			cout << "you're in room 6, where you encounter a monster! you can go north or south!";
 			getline(cin, input);
-			if (input == 'n')
+			if (input.compare("north") == 0)
 				room = 4;
-			if (input == 's')
+			if (input.compare("south") == 0)
 				room = 7;
 			break;
 
 		case 7:
 			cout << "you're in room 7! you can go north and west!";
 			getline(cin, input);
-			if (input == 'n')
+			if (input.compare("north") == 0)
 				room = 6;
-			if (input == 'w')
+			if (input.compare("west") == 0)
 				room = 8;
 			break;
 
 		case 8:
 			cout << "you're in room 8! you can go west or east!";
 			getline(cin, input);
-			if (input == 'w')
+			if (input.compare("west") == 0)
 				room = 9;
-			if (input == 'e')
+			if (input.compare("east") == 0)
 				room = 7;
 			break;
 
 		case 9:
 			cout << "you're in room 9, where you see a human! you can go north or south!";
 			getline(cin, input);
-			if (input == 'n')
+			if (input.compare("north") == 0)
 				room = 1;
-			if (input == 's')
+			if (input.compare("south") == 0)
 				room = 10;
 			break;
 
 		case 10:
 			cout << "you're in room 10, where you see a red apple! you can go north or south!";
 			getline(cin, input);
-			if (input == 'n')
+			if (input.compare("northt") == 0)
 				room = 9;
-			if (input == 's')
+			if (input.compare("south") == 0)
 				room = 11;
 
 			for (int i = 0; i < 5; i++)
@@ -134,18 +134,18 @@ int main() {
 		case 11:
 			cout << "you're in room 11! you encounter a monster! you need a key to unlock the door!";
 			getline(cin, input);
-			if (input == 'n')
+			if (input.compare("north") == 0)
 				room = 10;
-			if (input == 'e')
+			if (input.compare("east") == 0)
 				room = 12;
 			break;
 
 		case 12:
 			cout << "you're in room 12! congratulations!";
 			getline(cin, input);
-			if (input == 'n')
+			if (input.compare("north") == 0)
 				room = 1;
-			if (input == 'w')
+			if (input.compare("west") == 0)
 				room = 5;
 			break;
 		}
